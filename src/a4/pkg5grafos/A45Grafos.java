405f18ada9070954b1eeb2e0fd2e37b364ec6282
kevin <kevin@Kevin>
2025-11-24 18:25:42 -0600

    Primer commit de prueba al repositorio remoto.

---
+++ src/a4/pkg5grafos/A45Grafos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a4.pkg5grafos;
+
+/**
+ *
+ * @author kevin
+ */
+public class A45Grafos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
